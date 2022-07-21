62baabe2cfd5d1e6fc65d964ceb3e15bc4576eb1
cryuya <r-sasaki@croooober.co.jp>
2022-07-21 16:42:06 +0900

    rubocop -Aで修正されたファイルの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   validates :user_name, { presence: true }, { length: { in: 1..20 } }
   validates :title, { presence: true }, { length: { in: 1..20 } }
-  validates :message, { presence: true }
+  validates :message, { presence: true }, { length: { in: 1..200 } }
 end
---
+++ spec/post_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe Post, type: :model do
+  it 'るぼこっぷが動いていること'
+end
