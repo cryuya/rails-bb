553693bbb47f75dbcbf2fbecfa98c221b3687f9f
cryuya <r-sasaki@croooober.co.jp>
2022-07-21 15:57:51 +0900

    postモデルのバリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :user_name, { presence: true }, { length: { in: 1..20 } }
+  validates :title, { presence: true }, { length: { in: 1..20 } }
+  validates :message, { presence: true }
 end
